2273769cdf42f300880125f589d735dfdd0dd363
Kyaw Htay <kyawhtay@Kyaws-MacBook-Air.local>
2025-06-25 14:01:20 +0630

    Initial commit

--- frontend/index.php
+++ frontend/index.php
@@ -11,7 +11,7 @@
 
     <script>
         function fetchData() {
-            fetch('../backend/api.php')
+            fetch('http://localhost:9091/backend/api.php')
                 .then(response => response.json())
                 .then(data => {
                     document.getElementById('output').textContent = JSON.stringify(data, null, 4);
