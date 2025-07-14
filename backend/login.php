efa01ad64f96b241c6ffce68ebccc2f2f85d2738
hash1kyawhtay <hashonekyawhtay.com>
2025-07-14 16:50:36 +0630

    first commit

--- backend/login.php
+++ backend/login.php
@@ -1,6 +1,14 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Content-Type: application/json");
+
+// Optional: handle preflight request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 require 'db.php';
 
